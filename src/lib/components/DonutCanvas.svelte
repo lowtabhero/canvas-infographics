042a7be619e6f1c9e4eb0fd63549ee9216e9d1fd
Alexey <lowtabhero@mail.ru>
2026-02-19 20:03:43 +0100

    separate donut, add canvas intersection test

---
+++ src/lib/components/DonutCanvas.svelte
@@ -0,0 +1,116 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let canvas;
+	let currentRad = 0;
+
+	// const offset = -Math.PI / 2; // start from the top
+	const offset = 0; // start from the top
+	let canvasDiameter = 500;
+	let canvasCenter = canvasDiameter / 2;
+	let lineWidth = 125;
+
+	let { total, rows } = $props();
+	let sectors = [];
+
+	onMount(() => {
+		if (!canvas) return;
+
+		resetCanvas();
+
+		canvas.addEventListener('mousemove', handleMousemove);
+	});
+
+	$effect(() => {
+		resetCanvas();
+
+		rows.forEach((row) => {
+			draw(row);
+		});
+	});
+
+	function handleMousemove(event) {
+		console.log(event);
+		// if canvas is not on the left top
+		// const rect = canvas.getBoundingClientRect();
+		// console.log(rect);
+
+		const x = event.clientX;
+		const y = event.clientY;
+		const dx = x - canvasCenter;
+		const dy = y - canvasCenter;
+
+		const teta = Math.atan2(dy, dx);
+		console.log(teta);
+
+		const test = sectors[0];
+		if (test.start <= teta && test.end >= teta) console.log('intersecting first section');
+	}
+
+	function draw(obj) {
+		var ctx = canvas.getContext('2d');
+
+		ctx.lineWidth = lineWidth;
+		ctx.strokeStyle = obj.c;
+		let radians = (Math.PI * obj.p) / 50;
+		sectors.push({ ...obj, start: currentRad, end: currentRad + radians });
+
+		ctx.beginPath();
+		ctx.arc(
+			canvasDiameter / 2,
+			canvasDiameter / 2,
+			(canvasDiameter - lineWidth) / 2,
+			currentRad + offset,
+			currentRad + radians + offset,
+			false
+		);
+		ctx.stroke();
+
+		currentRad = currentRad + radians;
+	}
+
+	function initialDraw() {
+		var ctx = canvas.getContext('2d');
+
+		ctx.lineWidth = lineWidth;
+		ctx.strokeStyle = 'grey';
+		let radians = Math.PI * 2;
+
+		ctx.beginPath();
+		ctx.arc(
+			canvasDiameter / 2,
+			canvasDiameter / 2,
+			(canvasDiameter - lineWidth) / 2,
+			currentRad + offset,
+			currentRad + radians + offset,
+			false
+		);
+		ctx.stroke();
+	}
+
+	function resetCanvas() {
+		var ctx = canvas.getContext('2d');
+		ctx.clearRect(0, 0, canvasDiameter, canvasDiameter);
+		currentRad = 0;
+		sectors = [];
+		initialDraw();
+	}
+</script>
+
+<div class="donut">
+	<canvas bind:this={canvas} width={canvasDiameter} height={canvasDiameter}></canvas>
+	<div class="total">total: {total}</div>
+</div>
+
+<style lang="scss">
+	.donut {
+		position: relative;
+
+		> .total {
+			position: absolute;
+			left: 50%;
+			top: 50%;
+			transform: translateX(-50%) translateY(-50%);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,28 @@
 <script>
-	import { onMount } from 'svelte';
+	import DonutCanvas from '$lib/components/DonutCanvas.svelte';
 
-	let canvas, rowInput;
-	const offset = -Math.PI / 2; // start from top
-	let currentRad = 0;
+	let valueInput, colorInput;
 
-	let canvasDiameter = 500;
-	let lineWidth = 125;
-
-	let totalValue = 100;
-	let rows = $state([]);
 	let colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink'];
 
-	onMount(() => {
-		draw({ p: 100, c: 'grey' });
-	});
+	let totalValue = $state(100);
+	let rows = $state([]);
 
 	function handleAddRow() {
-		const v = rowInput.value;
+		const v = valueInput.value;
 		const p = (totalValue / 100) * v;
-		const row = { p: p, v: rowInput.value, c: colors[rows.length] };
-		rows.push(row);
-		draw(row);
-	}
+		const c = colorInput.value;
+		if (!v || !p || !c) return;
 
-	function drawRows() {
-		// reset canvas and draw rows
-	}
-
-	function draw(obj) {
-		var ctx = canvas.getContext('2d');
-
-		ctx.lineWidth = lineWidth;
-		ctx.strokeStyle = obj.c;
-		let radians = (Math.PI * obj.p) / 50;
-
-		ctx.beginPath();
-		ctx.arc(
-			canvasDiameter / 2,
-			canvasDiameter / 2,
-			(canvasDiameter - lineWidth) / 2,
-			currentRad + offset,
-			currentRad + radians + offset,
-			false
-		);
-		ctx.stroke();
-
-		currentRad = currentRad + radians;
+		rows.push({ p: p, v: v, c: c });
 	}
 </script>
 
 <div class="container">
-	<div class="donut">
-		<canvas bind:this={canvas} width={canvasDiameter} height={canvasDiameter}></canvas>
-		<div class="total">total: {totalValue}</div>
-	</div>
+	<DonutCanvas total={totalValue} {rows} />
 	<div class="input-container">
-		<input type="text" placeholder="Input row here" bind:this={rowInput} />
+		<input type="number" placeholder="Input number here" bind:this={valueInput} />
+		<input type="color" bind:this={colorInput} />
 		<button onclick={handleAddRow}>Add row</button>
 	</div>
 </div>
@@ -68,21 +34,11 @@
 		flex-wrap: wrap;
 		gap: 40px;
 
-		> .donut {
-			position: relative;
-
-			> .total {
-				position: absolute;
-				left: 50%;
-				top: 50%;
-				transform: translateX(-50%) translateY(-50%);
-			}
-		}
-
 		> .input-container {
 			flex-grow: 1;
 			flex-basis: 300px;
 			border: 1px solid black;
+			padding: 1rem;
 		}
 	}
 </style>
